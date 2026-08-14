f7278ed7c0dc8a9578be36a6b026f3edb720cdfb
Vince Odhiambo <architectvince7@gmail.com>
2026-08-14 17:31:54 +0300

    Add Appwrite client for customer accounts

---
+++ lib/appwrite/client.ts
@@ -0,0 +1,9 @@
+import { Client, Account, Databases } from 'appwrite'
+
+const client = new Client()
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '')
+
+export const account = new Account(client)
+export const databases = new Databases(client)
+export default client
